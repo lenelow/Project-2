246ed72662df796c5b0001bd396b7a4687ef81e7
Leah Enelow <lenelow@students.terpmail.edu>
2018-06-20 16:53:25 -0400

    finishing up db files

--- db/profiles.seeder.js
+++ db/profiles.seeder.js
@@ -1,52 +1,31 @@
-const Dog = require('../models/profiles')
+const Profile = require('../models/profiles')
 const bcrypt = require('bcrypt-nodejs')
 
 
 //use model to get profile from database
 
+  Profile.find({}).remove(() => {
+      let Lisa = Profile.create({
+          user: user.id,
+          location: 20815,
+          bio: "I'm new in town and would love to meet a fellow dog lover so my dog has someone to play with!",
+          dogInterest: true,
+          humanInterest: true
+        }).then(() => {
+          // our second .then ensures that this code runs after all the previous code is done
+          user.save(err => console.log(err))
+          // .save writes the record to the database.
+        })
+        async shouldRun() {
+            return User.count()
+              .exec()
+              .then(count => count === 0);
+          },
+        
+          async run() {
+            return User.create(data);
+          }
+      })
 
-module.exports = {
-    show: (req, res) => {
-      Tweet.findOne({ _id: req.params.id })
-        .populate("author")
-        .exec(function(err, tweet) {
-          Comment.populate(tweet.comments, { path: "author" }, function(
-            err,
-            comments
-          ) {
-            tweet.comments = comments;
-            res.render("index", tweet);
-          });
-        });
-    },
-    new: (req, res) => {
-      User.find({}).then(users => {
-        res.render("index", { users });
-      });
-    },
-    create: (req, res) => {
-      Tweet.create({
-        content: req.body.tweet.content,
-        author: req.body.author
-      }).then(tweet => {
-        User.findOne({ _id: req.body.author }).then(user => {
-          user.tweets.push(tweet);
-          user.save(err => {
-            res.redirect(`/tweet/${tweet._id}`);
-          });
-        });
-      });
-    },
-    update: (req, res) => {
-      let { content, author } = req.body;
-      Tweet.findOne({ _id: req.params.id }).then(tweet => {
-        tweet.comments.push({
-          content,
-          author
-        });
-        tweet.save(err => {
-          res.redirect(`/tweet/${tweet._id}`);
-        });
-      });
-    }
-  };
+      export default UsersSeeder;
+   
--- db/users.seeder.js
+++ db/users.seeder.js
@@ -1,123 +1,47 @@
-const User = require("../models/User");
-const { Tweet } = require("../models/Tweet");
-const bcrypt = require("bcrypt-nodejs");
+const User = require('../models/users');
+const Dog = require('../models/dogs');
+const bcrypt = require('bcrypt-nodejs');
 
 //users example number 1
 const createPassword = password =>
   bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 
   User.find({}).remove(() => {
-    Tweet.find({}).remove(() => {
-      let bugs = User.create({
-        local: {
-          email: "bugsbunny@gmail.com",
-          password: createPassword("bugsbunny")
-        }
-      }).then(user => {
+    Dog.find({}).remove(() => {
+      let Bob = User.create({
+          email: "bob@gmail.com",
+          password: createPassword("bob")
+        }).then(user => {
         // user is the user object we just made using .create
   
         // Promise.all will run all of the provided functions simultaneously, and return when they're finished
-        Promise.all([
-          Tweet.create({
-            content: "eh, what's up doc?",
-            author: user._id
-          }).then(tweet => {
-            user.tweets.push(tweet);
-          }),
-          Tweet.create({
-            content: "That's all, folks!",
-            author: user._id
-          }).then(tweet => {
-            user.tweets.push(tweet);
+          Dog.create({
+            name: Max,
+            weight: "10 pounds",
+            breed: "toy poodle",
+            avatar: "dhlahgdlahds.jpeg",
+            additionalInfo: "I'm new in town, and I would love a dog to play with. I'm very friendly and get along well with all types of dogs.",
+            user: user.id
+          }).then(profile => {
+            user.dogs.push(dog);
           })
-        ]).then(() => {
+        .then(() => {
           // our second .then ensures that this code runs after all the previous code is done
           user.save(err => console.log(err));
           // .save writes the record to the database.
         });
       });
     });
+    async shouldRun() {
+        return User.count()
+          .exec()
+          .then(count => count === 0);
+      },
+    
+      async run() {
+        return User.create(data);
+      }
   });
 
-  //users example number 2 
-  var Seeder = require('../../../').Seeder;
-var User = require('../server/models').User;
 
-var data = [
-  {
-    email: 'user1@gmail.com',
-    password: '123123',
-    passwordConfirmation: '123123',
-    isAdmin: true,
-  },
-  {
-    email: 'user2@gmail.com',
-    password: '123123',
-    passwordConfirmation: '123123',
-    isAdmin: false,
-  },
-  {
-    email: 'user3@gmail.com',
-    password: '123123',
-    passwordConfirmation: '123123',
-    isAdmin: false,
-  },
-  {
-    email: 'user4@gmail.com',
-    password: '123123',
-    passwordConfirmation: '123123',
-    isAdmin: false,
-  },
-  {
-    email: 'user5@gmail.com',
-    password: '123123',
-    passwordConfirmation: '123123',
-    isAdmin: false,
-  },
-];
-
-var UsersSeeder = Seeder.extend({
-  shouldRun: function() {
-    return User.count()
-      .exec()
-      .then(count => count === 0);
-  },
-  run: function() {
-    return User.create(data);
-  },
-});
-
-module.exports = UsersSeeder;
-
-//number 3
-import { Seeder } from "mongoose-data-seed";
-import { User } from "../server/models";
-
-const data = [
-  {
-    email: "user1@gmail.com",
-    password: "123123",
-    password_confirmation: "123123",
-    isAdmin: true
-  },
-  {
-    email: "user2@gmail.com",
-    password: "123123",
-    password_confirmation: "123123",
-    isAdmin: false
-  }
-];
-
-class UsersSeeder extends Seeder {
-  async shouldRun() {
-    return User.count()
-      .exec()
-      .then(count => count === 0);
-  }
-
-  async run() {
-    return User.create(data);
-  }
-}
-
-export default UsersSeeder;
+    export default UsersSeeder;
