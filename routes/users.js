3246b54167f6e03b75a07c0627994ee36b26d6f0
Leah Enelow <lenelow@students.terpmail.edu>
2018-06-25 19:57:35 -0400

    timeout

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/users')
-const multer = require('multer')
-const upload = multer({ dest: './public/images' })
 
 router.get('/', userController.goHome)// go home
 router.get('/login', userController.loginForm)// render login form
 router.post('/', userController.login)// handle login inf(home page or error)
 router.get('/signup', userController.signupForm)// render signup form
-router.post('/signup', upload.single('avatar'), userController.signup)// add new account to database and redirect to profile create
+router.post('/signup', userController.signup)// add new account to database and redirect to profile create
 router.get('/logout', userController.logout)// log out
 
 // search
