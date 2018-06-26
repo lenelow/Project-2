008633dc5e4766edf933a1f9a7d5bdfc2cfac444
Leah Enelow <lenelow@students.terpmail.edu>
2018-06-25 20:57:17 -0400

    fixing db problem

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV === 'production') {
     mongoose.connect(process.env.MLAB_URL)
 } else {
-mongoose.connect('mongodb://leah:chance04@ds015760.mlab.com:15760/doggydate')
+mongoose.connect('mongodb://isaac:doggy01ds015760.mlab.com:15760/doggydate')
 }
 
 mongoose.Promise = Promise;
